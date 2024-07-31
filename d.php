a9e4881d5ec3bdc969e1cd72f446e4bf95ca00e9
jas-wang <18380358053@163.com>
2024-07-31 13:07:39 +0800

    Create d.php

---
+++ d.php
@@ -0,0 +1,12 @@
+<?php
+use Controller/abc.php;
+class c{
+public function d($a){
+
+  $fg = $a;
+}
+  public function adasdasdadsadasdasdadsadasdasdadsadasdasdadsadasdasdadsadasdasdadsadasdasdadsadasdasdadsadasdasdads($a){
+
+    $test = $bsd;
+  }
+}
